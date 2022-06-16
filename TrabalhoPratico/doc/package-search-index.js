3c381ed2ba81afba696d4659a55a0948290a5c0e
Pedro Morais <35313213+pedrojfmorais@users.noreply.github.com>
2022-06-16 17:39:33 +0100

    Delete TrabalhoPratico/doc directory

--- TrabalhoPratico/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pt.isec.pa.apoio_poe.model.data"},{"l":"pt.isec.pa.apoio_poe.model.data.pessoas"},{"l":"pt.isec.pa.apoio_poe.model.data.pessoas.alunos"},{"l":"pt.isec.pa.apoio_poe.model.data.propostas"},{"l":"pt.isec.pa.apoio_poe.model.errorHandling"},{"l":"pt.isec.pa.apoio_poe.model.fsm"}];updateSearchResults();
